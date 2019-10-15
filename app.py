48b6c98a38b50c7bbfabd4838c13eb0142f12934
ramadityanurjana <ramadityanurjana@gmail.com>
2019-10-15 14:53:54 +0800

    TIBOT

--- app.py
+++ app.py
@@ -13,8 +13,18 @@ PORT = int(os.environ.get("PORT", 5000))
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
+    pesan = data['queryResult']['queryText']
     intent_name = data.get("queryResult").get("intent").get("displayName")
     print(data)
+    connection = pymysql.connect(host='db4free.net', user='ramaditya', password='osesehat019', db='tibotdb',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO tb_inbox(pesan,date) VALUES (%s,%s)"
+            cursor.execute(sql, (date.today().strftime("%Y-%m-%d"), pesan))
+        connection.commit()
+    finally:
+        connection.close()
 
     if intent_name == "order":
         return order(data)
